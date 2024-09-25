5b0ec01526daadb623aa2495565023d218cd3e1d
Franc <107664564+FrancGDev@users.noreply.github.com>
2024-09-25 19:02:00 -0300

    minor fix

--- src/components/scripts.js
+++ src/components/scripts.js
@@ -29,3 +29,29 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+const carousel = document.getElementById('carousel');
+const slides = carousel.children.length;
+let index = 0;
+
+function moveCarousel() {
+    index = (index + 1) % slides;
+    carousel.style.transition = 'transform 0.5s ease-in-out';
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+const intervalId = setInterval(moveCarousel, 10000);
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+    clearInterval(intervalId);
+    moveCarousel();
+});
+
+document.getElementById('prevBtn').addEventListener('click', () => {
+    clearInterval(intervalId);
+    index = (index - 1 + slides) % slides;
+    carousel.style.transition = 'transform 0.5s ease-in-out';
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+});
